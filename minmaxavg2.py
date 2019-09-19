b85c0e078deea6dfd4e99b0b5fbf2e48077c0c1d
bmar94 <53377522+bmar94@users.noreply.github.com>
2019-09-19 10:41:25 +0200

    Add files via upload

---
+++ minmaxavg2.py
@@ -0,0 +1,20 @@
+numbers = [-5, 23, 0, -9, 12, 99, 105, -43]
+
+min = numbers[0]
+max = numbers[0]
+value = 0
+
+for i in numbers:
+    if i <= min:
+        min = i
+
+for j in numbers:
+    if j >= max:
+        max = j
+
+for k in numbers:
+    value += k
+
+avg = value / len(numbers)
+
+print("Min: {}\nMax: {}\nAvg: {}".format(min, max, avg))
